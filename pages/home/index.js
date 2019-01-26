2dc29f6897562330b44bc925b649405d2797c35c
AbbottZF <39108584+AbbottZF@users.noreply.github.com>
2019-01-26 17:10:11 +0800

    Initial commit

---
+++ pages/home/index.js
@@ -0,0 +1,33 @@
+const app = getApp();
+
+Page({
+  data:{
+    text:'123',
+    CustomBar: 0, iconList: [{
+      icon: 'cardboardfill',
+      color: 'red',
+      badge: 120,
+      name: 'VR'
+    }, {
+      icon: 'recordfill',
+      color: 'orange',
+      badge: 1,
+      name: '录像'
+    }, {
+      icon: 'picfill',
+      color: 'yellow',
+      badge: 0,
+      name: '图像'
+    }, {
+      icon: 'noticefill',
+      color: 'olive',
+      badge: 22,
+      name: '通知'
+    }, {
+      icon: 'upstagefill',
+      color: 'cyan',
+      badge: 0,
+      name: '排行榜'
+    }],
+  }
+});
